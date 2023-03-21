cb82570d708adf43dbc7ae4bda69bdd3789413ea
Ashish Mahadik <ashishmahadik3942@gmail.com>
2023-03-21 14:30:55 +0530

    update

--- index.js
+++ index.js
@@ -1,110 +1,121 @@
-const fullname = document.getElementById("fullname");
-const password = document.getElementById("password");
-const number = document.getElementById("number");
-const url = document.getElementById("url");
-const month = document.getElementById("month");
-const per = document.getElementById("per");
-const uni = document.getElementById("uni");
-const time = document.getElementById("time");
-const week = document.getElementById("week");
-const date = document.getElementById("date");
-const email = document.getElementById("email");
-const gender = document.getElementById("gender");
-const range = document.getElementById("range");
-const file = document.getElementById("file");
-const color = document.getElementById("color");
-const hidden = document.getElementById("hidden");
-const radio = document.querySelector('input[name="radio"]:checked');   
-const angular = document.getElementById("angular");
-const react = document.getElementById("react");
-const reset = document.getElementById("reset");
-const submit = document.getElementById("submit1");
-
-reset.addEventListener('click', ()=>{
-    location.reload()
-})
-
-
-
-submit.addEventListener('mousedown', (e)=>{
-
-    
-
-    if(fullname.value=== ""){
-        console.log("Enter Name")
-        alert("Enter Name")
+var fullname = document.getElementById("fullname");
+var password = document.getElementById("password");
+var number = document.getElementById("number");
+var url = document.getElementById("url");
+var month = document.getElementById("month");
+var per = document.getElementById("per");
+var uni = document.getElementById("uni");
+var time = document.getElementById("time");
+var week = document.getElementById("week");
+var date = document.getElementById("date");
+var email = document.getElementById("email");
+var gender = document.getElementById("gender");
+var range = document.getElementById("range");
+var file = document.getElementById("file");
+var color = document.getElementById("color");
+var hidden = document.getElementById("hidden");
+var radio = document.querySelector('input[name="radio"]:checked');
+var angular = document.getElementById("angular");
+var react = document.getElementById("react");
+var reset = document.getElementById("reset");
+var submit = document.getElementById("submit1");
+reset.addEventListener('hover', function () {
+    location.reload();
+});
+submit.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+    var passRegex = /^[A-Za-z]{1,}\d{1,}/;
+    if (fullname.value.length < 5) {
+        alert("Enter Name");
     }
-    
-    else if(password.value===""){
-        alert("Enter password")
-        console.log("Enter password")
+    else if (password.value.length < 8 || !passRegex.test(password.value)) {
+        alert("Enter valid password");
     }
-
-    else if(number.value===""){
-        alert("Enter number")
-        console.log("Enter number")
+    else if (number.value === "") {
+        alert("Enter number");
+        console.log("Enter number");
     }
-
-    else if(url.value===""){
-        alert("Enter Url")
-        console.log("Enter Url")
+    else if (url.value === "") {
+        alert("Enter Url");
+        console.log("Enter Url");
     }
-
-    else if(month.value===""){
-        alert("Select month")
-        console.log("Select month")
+    else if (month.value === "") {
+        alert("Select month");
+        console.log("Select month");
     }
-
-    else if(per.value===""){
-        alert("Enter percentage")
-        console.log("Enter percentage")
+    else if (per.value === "") {
+        alert("Enter percentage");
+        console.log("Enter percentage");
     }
-
-    else if(uni.value===""){
-        alert("Enter University name")
-        console.log("Enter University name")
+    else if (uni.value === "") {
+        alert("Enter University name");
+        console.log("Enter University name");
     }
-
-    else if(time.value===""){
-        alert("Select time")
-        console.log("Select time")
+    else if (time.value === "") {
+        alert("Select time");
+        console.log("Select time");
     }
-    
-    else if(week.value===""){
-        alert("Select week")
-        console.log("Select week")
+    else if (week.value === "") {
+        alert("Select week");
+        console.log("Select week");
     }
-
-    else if(date.value===""){
-        alert("Enter birth date")
-        console.log("Enter birth date")
+    else if (date.value === "") {
+        alert("Enter birth date");
+        console.log("Enter birth date");
     }
-
-    else if(email.value===""){
-        alert("Enter email")
-        console.log("Enter email")
+    else if (email.value === "") {
+        alert("Enter email");
+        console.log("Enter email");
     }
-    
-    else if(file.value===""){
-        alert("Select file")
-        console.log("Select file")
+    else if (file.value === "") {
+        alert("Select file");
+        console.log("Select file");
     }
-
-    else if(angular.checked != true && react.checked != true){
-        alert("Select either option from skills")
-        console.log("Select either option from skills")
+    else if (angular.checked != true && react.checked != true) {
+        alert("Select either option from skills");
+        console.log("Select either option from skills");
     }
-
-    else{
-        console.log(`Name:${fullname.value} Password:${password.value} tel:${per.value}
-        url:${url.value} Month:${month.value} number:${number.value} textarea:${uni.value} 
-        time:${time.value} week:${week.value} date:${date.value} email:${email.value} 
-        select:${gender.value} range:${range.value} file:${file.value} 
-        color:${color.value} radio:${radio.value} checkbox1:${angular.checked} 
-        checkbox2:${react.checked}`)
-        event.preventDefault();
-        alert(`Successful ${fullname.value}`)
+    if (true) {
+        // console.log(`Name:${fullname.value} Password:${password.value} tel:${per.value}
+        // url:${url.value} Month:${month.value} number:${number.value} textarea:${uni.value} 
+        // time:${time.value} week:${week.value} date:${date.value} email:${email.value} 
+        // select:${gender.value} range:${range.value} file:${file.value} 
+        // color:${color.value} radio:${radio.value} checkbox1:${angular.checked} 
+        // checkbox2:${react.checked}`)
+        // event.preventDefault();
+        // alert(`Successful ${fullname.value}`)
+        var person = {
+            name: fullname.value,
+            fullname: fullname.value,
+            password: password.value,
+            number: number.value,
+            email: email.value,
+            month: month.value,
+            per: per.value,
+            uni: uni.value,
+            time: time.value,
+            week: week.value,
+            date: date.value,
+            gender: gender.value,
+            range: range.value,
+            file: file.value,
+            color: color.value,
+            hidden: hidden.value,
+            radio: radio.value,
+            angular: angular.value,
+            react: react.value,
+            reset: reset.value,
+            url: url.value
+        };
+        $.ajax({
+            url: 'https://winter-summer-sceptre.glitch.me/submit',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                console.log(data);
+            },
+            data: JSON.stringify(person)
+        });
     }
-
-    event.preventDefault();
-})
+});
---
+++ index.ts
@@ -0,0 +1,144 @@
+const fullname = document.getElementById("fullname") as HTMLInputElement;
+const password = document.getElementById("password") as HTMLInputElement;
+const number = document.getElementById("number") as HTMLInputElement;
+const url = document.getElementById("url") as HTMLInputElement;
+const month = document.getElementById("month") as HTMLInputElement;
+const per = document.getElementById("per") as HTMLInputElement;
+const uni = document.getElementById("uni") as HTMLInputElement;
+const time = document.getElementById("time") as HTMLInputElement;
+const week = document.getElementById("week") as HTMLInputElement;
+const date = document.getElementById("date") as HTMLInputElement;
+const email = document.getElementById("email") as HTMLInputElement;
+const gender = document.getElementById("gender") as HTMLInputElement;
+const range = document.getElementById("range") as HTMLInputElement;
+const file = document.getElementById("file") as HTMLInputElement;
+const color = document.getElementById("color") as HTMLInputElement;
+const hidden = document.getElementById("hidden") as HTMLInputElement;
+const radio = document.querySelector('input[name="radio"]:checked') as HTMLInputElement;   
+const angular = document.getElementById("angular") as HTMLInputElement;
+const react = document.getElementById("react") as HTMLInputElement;
+const reset = document.getElementById("reset") as HTMLInputElement;
+const submit = document.getElementById("submit1") as HTMLInputElement;
+
+reset.addEventListener('hover', ()=>{
+    location.reload()
+})
+
+
+
+submit.addEventListener('mousedown', (e)=>{
+    e.preventDefault();
+    let passRegex = /^[A-Za-z]{1,}\d{1,}/;
+    let numberRegex = /\d{10}/;
+
+    if(fullname.value.length < 5){
+        alert("Enter valid Name")
+    }
+    
+    else if(password.value.length < 8 || !passRegex.test(password.value)){
+        alert("Enter valid password")
+    }
+
+    else if(number.value.length !== 10 ||  !numberRegex.test(number.value)){
+        alert("Enter valid number")
+    }
+
+    else if(url.value===""){
+        alert("Enter Url")
+        console.log("Enter Url")
+    }
+
+    else if(month.value===""){
+        alert("Select month")
+        console.log("Select month")
+    }
+
+    else if(per.value===""){
+        alert("Enter percentage")
+        console.log("Enter percentage")
+    }
+
+    else if(uni.value===""){
+        alert("Enter University name")
+        console.log("Enter University name")
+    }
+
+    else if(time.value===""){
+        alert("Select time")
+        console.log("Select time")
+    }
+    
+    else if(week.value===""){
+        alert("Select week")
+        console.log("Select week")
+    }
+
+    else if(date.value===""){
+        alert("Enter birth date")
+        console.log("Enter birth date")
+    }
+
+    else if(email.value===""){
+        alert("Enter email")
+        console.log("Enter email")
+    }
+    
+    else if(file.value===""){
+        alert("Select file")
+        console.log("Select file")
+    }
+
+    else if(angular.checked != true && react.checked != true){
+        alert("Select either option from skills")
+        console.log("Select either option from skills")
+    }
+
+    if(true){
+        // console.log(`Name:${fullname.value} Password:${password.value} tel:${per.value}
+        // url:${url.value} Month:${month.value} number:${number.value} textarea:${uni.value} 
+        // time:${time.value} week:${week.value} date:${date.value} email:${email.value} 
+        // select:${gender.value} range:${range.value} file:${file.value} 
+        // color:${color.value} radio:${radio.value} checkbox1:${angular.checked} 
+        // checkbox2:${react.checked}`)
+        // event.preventDefault();
+        // alert(`Successful ${fullname.value}`)
+
+
+        let person = {
+            name: fullname.value,
+            fullname: fullname.value,
+            password: password.value,
+            number: number.value, 
+            email: email.value,
+            month: month.value, 
+            per: per.value, 
+            uni: uni.value,
+            time: time.value, 
+            week: week.value, 
+            date: date.value, 
+            gender: gender.value, 
+            range: range.value, 
+            file: file.value, 
+            color: color.value, 
+            hidden: hidden.value, 
+            radio: radio.value, 
+            angular: angular.value, 
+            react: react.value, 
+            reset: reset.value,
+            url: url.value 
+        }
+
+        
+        $.ajax({
+            url: 'https://winter-summer-sceptre.glitch.me/submit',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                console.log(data)
+            },
+            data: JSON.stringify(person)
+        });
+    }
+   
+})
